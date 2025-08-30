ae20623d927f05f6a9af7005f60391768f261feb
arthika333 <arthikaparivalar@gmail.com>
2025-08-30 22:16:44 +0400

    Update predicting.py

--- src/predicting.py
+++ src/predicting.py
@@ -2,9 +2,10 @@ import tensorflow as tf
 from utils import predictimages
 
 classnames = [str(i) for i in range(10)]
-model = tf.keras.models.load_model('/content/savedmodel.keras')
+model = tf.keras.models.load_model('/model/savedmodel.keras')
 
 #list your custom images file paths
 images = []
 predictimages(model,images,classnames)
      
+
